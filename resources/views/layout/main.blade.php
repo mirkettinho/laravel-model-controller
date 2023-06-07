610972da68399a8642a8a5c798d6fe01c62e5bfd
mirkettinho <mirkodifranco30@gmail.com>
2023-06-07 15:42:43 +0200

    first commit

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>document</title>
+
+        {{-- Includiamo gli assets con la direttiva @vite --}}
+        @vite('resources/js/app.js')
+    </head>
+    <body>
+
+
+        <header>
+
+          @include("partials.header")
+        </header>
+
+
+        <main>
+
+            @yield("content")
+
+        </main>
+
+
+        <footer>
+
+          @include("partials.footer")
+        </footer>
+
+    </body>
+</html>
